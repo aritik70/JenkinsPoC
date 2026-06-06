b201ed7e19b8bc046e474c0004578425812d77c0
aritik70 <aritik243@gmail.com>
2026-06-06 15:03:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello from Jenkins CI/CD Pipeline!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def hello():
+    return "Hello from AWS CI/CD 🚀"
